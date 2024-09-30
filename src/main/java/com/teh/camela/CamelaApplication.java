bf76c8a4cce93add8dc43a483034f72136fd9875
Luiz Fernando Areas <lfernandoareas@gmail.com>
2024-09-30 06:25:13 -0300

    first commit camel service A

---
+++ src/main/java/com/teh/camela/CamelaApplication.java
@@ -0,0 +1,13 @@
+package com.teh.camela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelaApplication.class, args);
+	}
+
+}
